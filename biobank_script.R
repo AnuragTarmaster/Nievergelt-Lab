1e67dd9dbe405e58989a18fe42d727eb0336a058
Anurag Tarmaster <anurag.tarmaster@gmail.com>
2022-12-13 16:46:04 -0800

    Update biobank_script.R
    changed tab spacing

--- biobank_script.R
+++ biobank_script.R
@@ -55,64 +55,64 @@ sheet_names <- c('Plasma_V1',
 								 
 # iterate through each sheet in the sample map
 for (x in sheet_names) {																												
-  i <- 0
+	i <- 0
 	
 	# make new dataframe called x for each sheet
-  assign(x, read_excel('biobank_boxes_10_25_22.xlsx', sheet = x))  
+	assign(x, read_excel('biobank_boxes_10_25_22.xlsx', sheet = x))  
 	# iterate through each row of x
-  while (i <= nrow(eval(parse(text = x)))) {
+	while (i <= nrow(eval(parse(text = x)))) {
 		# locate each boxkey in x and define it as a 10x10 dataframe
-    boxkey = eval(parse(text = x))[(i+3):(i+12), 2:11]
+		boxkey = eval(parse(text = x))[(i+3):(i+12), 2:11]
 		# identify boxkey number, skip rows that don't have a boxkey
-    boxkey_num <- as.numeric(unname(unlist(eval(parse(text = x))[(i+1),1])))
-    # make sure there is a boxkey here
+		boxkey_num <- as.numeric(unname(unlist(eval(parse(text = x))[(i+1),1])))
+		# make sure there is a boxkey here
 		if (is.na(boxkey_num) == FALSE) {
-	  numberOfBoxkeys <- numberOfBoxkeys + 1
+		numberOfBoxkeys <- numberOfBoxkeys + 1
 			for (a in 1:nrow(boxkey)) {
-        for (b in 1:ncol(boxkey)) {
+				for (b in 1:ncol(boxkey)) {
 					# ignore slots without samples
-          if (is.na(unname(unlist(boxkey[a,b]))) == FALSE) {
+					if (is.na(unname(unlist(boxkey[a,b]))) == FALSE) {
 						# add the sample name to a vector
-            ID_visit <- append(ID_visit, unname(unlist(boxkey[a,b])))
+						ID_visit <- append(ID_visit, unname(unlist(boxkey[a,b])))
 						# add the row to a vector
-            row <- append(row, a)
+						row <- append(row, a)
 						# add the column to a vector
-            column <- append(column, b)
+						column <- append(column, b)
 						# add the boxkey number to a vector
-            boxkey_nums <- append(boxkey_nums, boxkey_num)
+						boxkey_nums <- append(boxkey_nums, boxkey_num)
 						# add tissue type to a vector
-					  tissue <- append(tissue, x)
-          }
-        }
-      }
-    }
+						tissue <- append(tissue, x)
+					}
+				}
+			}
+		}
 		# move to the next boxkey
-    i = i + 14
-  }
-  i <- 0
+		i = i + 14
+	}
+	i <- 0
   
 	# only some sheets have another 'column' of boxkeys
-  if (x != 'Plasma_V2_temp' & x != 'WB_V1' & x != 'LPS_V3') {
-    while (i < nrow(eval(parse(text = x)))) {
-      boxkey = eval(parse(text = x))[(i+3):(i+12), 14:23]
-      boxkey_num <- as.numeric(unname(unlist(eval(parse(text = x))[(i+1),13])))
-      if (is.na(boxkey_num) == FALSE) {
-	    numberOfBoxkeys <- numberOfBoxkeys + 1
-        for (a in 1:nrow(boxkey)) {
-          for (b in 1:ncol(boxkey)) {
+	if (x != 'Plasma_V2_temp' & x != 'WB_V1' & x != 'LPS_V3') {
+		while (i < nrow(eval(parse(text = x)))) {
+			boxkey = eval(parse(text = x))[(i+3):(i+12), 14:23]
+			boxkey_num <- as.numeric(unname(unlist(eval(parse(text = x))[(i+1),13])))
+			if (is.na(boxkey_num) == FALSE) {
+				numberOfBoxkeys <- numberOfBoxkeys + 1
+				for (a in 1:nrow(boxkey)) {
+					for (b in 1:ncol(boxkey)) {
 						if (is.na(unname(unlist(boxkey[a,b]))) == FALSE) {
-              ID_visit <- append(ID_visit, unname(unlist(boxkey[a,b])))
-              row <- append(row, a)
-              column <- append(column, b)
-              boxkey_nums <- append(boxkey_nums, boxkey_num)
+							ID_visit <- append(ID_visit, unname(unlist(boxkey[a,b])))
+							row <- append(row, a)
+							column <- append(column, b)
+							boxkey_nums <- append(boxkey_nums, boxkey_num)
 							tissue <- append(tissue, x)
 						}
-          }
-        }
-      }
-      i = i + 14
-    }
-  }
+					}
+				}
+			}
+			i = i + 14
+		}
+	}
 }
 # write a dataframe with the desired columns and make a new excel spreadsheet
 df <- data.frame(boxkey_nums, tissue, ID_visit, row, column)
